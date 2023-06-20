b1ca34b651110220ed980d0ac810f7b139e70740
rnimisha <nimisaraut@gmail.com>
2023-06-20 15:01:50 +0545

    Update model to store multiple image

---
+++ prisma/migrations/20230620091538_added_image_model/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `image` on the `Post` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "image";
+
+-- CreateTable
+CREATE TABLE "PostImage" (
+    "id" SERIAL NOT NULL,
+    "basename" TEXT NOT NULL,
+    "postId" INTEGER NOT NULL,
+
+    CONSTRAINT "PostImage_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "PostImage" ADD CONSTRAINT "PostImage_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
